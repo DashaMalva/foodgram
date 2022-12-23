7c18a8585bb10ad034de3d7d88d68ce5ce84d209
Дарья Матвиевская <dollymo015@gmail.com>
2022-12-23 23:40:36 +0300

    Flake 8

--- backend_foodgram/api/urls.py
+++ backend_foodgram/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet, IngredientViewSet, RecipeViewSet, TagViewSet
+from .views import IngredientViewSet, RecipeViewSet, TagViewSet, UserViewSet
 
 router = DefaultRouter()
 
--- backend_foodgram/api/views.py
+++ backend_foodgram/api/views.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
 
 from django.contrib.auth import get_user_model
-from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
